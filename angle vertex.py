a04637fd738f68065674fc360dd3ba6a65e5613d
eureca114 <107104321+eureca114@users.noreply.github.com>
2025-06-01 15:50:01 +0530

    Update angle vertex.py

--- angle vertex.py
+++ angle vertex.py
@@ -2,9 +2,11 @@ from machine import *
 from time import *
 x=Pin(12,Pin.OUT)
 x=PWM(x)
+y=PWM(Pin(2,Pin.OUT))
 z=1
 for i in range(17):
     sleep(0.5)
     print(z)
     x.duty_u16(z-1)
+    y.duty_u16(z-1)
     z*=2
